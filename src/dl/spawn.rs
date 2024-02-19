c6cff5ad66d9c2c220b252298751b38dda09fc0e
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-02-19 12:48:10 +0200

    implement async process spawning

---
+++ src/dl/spawn.rs
@@ -0,0 +1,25 @@
+use std::process::{Command, Output};
+use tokio::task::{spawn_blocking, JoinError};
+
+pub enum SpawnError {
+    CommandError(std::io::Error),
+    SpawnBlockingError(JoinError)
+}
+
+pub async fn spawn_process(program: String, args: Vec<String>) -> Result<Output, SpawnError> {
+    let output = spawn_blocking(move || {
+        let output = Command::new(program)
+            .args(args)
+            .output();
+        
+        match output {
+            Ok(output) => Ok(output),
+            Err(e) => Err(SpawnError::CommandError(e))
+        }
+    }).await;
+
+    match output {
+        Ok(output) => output,
+        Err(e) => Err(SpawnError::SpawnBlockingError(e))
+    }
+}
