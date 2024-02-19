5f3f2096e814996f1ce6724af447dd6252cd2fe9
mykola2312 <nikolay_tihonov@i.ua>
2024-02-19 12:48:40 +0200

    cargo fmt

--- src/dl/spawn.rs
+++ src/dl/spawn.rs
@@ -3,23 +3,22 @@ use tokio::task::{spawn_blocking, JoinError};
 
 pub enum SpawnError {
     CommandError(std::io::Error),
-    SpawnBlockingError(JoinError)
+    SpawnBlockingError(JoinError),
 }
 
 pub async fn spawn_process(program: String, args: Vec<String>) -> Result<Output, SpawnError> {
     let output = spawn_blocking(move || {
-        let output = Command::new(program)
-            .args(args)
-            .output();
-        
+        let output = Command::new(program).args(args).output();
+
         match output {
             Ok(output) => Ok(output),
-            Err(e) => Err(SpawnError::CommandError(e))
+            Err(e) => Err(SpawnError::CommandError(e)),
         }
-    }).await;
+    })
+    .await;
 
     match output {
         Ok(output) => output,
-        Err(e) => Err(SpawnError::SpawnBlockingError(e))
+        Err(e) => Err(SpawnError::SpawnBlockingError(e)),
     }
 }
