d8ea0e74903afe67595409c7ec1c314a3841e292
mykola2312 <nikolay_tihonov@i.ua>
2024-02-21 18:42:09 +0200

    do renaming for command handlers so it wouldn't conflict with other functions

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,15 @@
 use dotenv::dotenv;
-use std::env;
 
 mod bot;
 use bot::bot::bot_main;
 
 mod dl;
-use dl::ffmpeg::FFMpeg;
-use dl::yt_dlp::YtDlp;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenv().ok();
+    //dotenv::from_filename(".env.test").ok();
 
-    let info = YtDlp::load_info(env::var("TEST_URL")?.as_str())
-        .await
-        .expect("load_info");
-    for format in &info.formats {
-        println!("{}", format);
-    }
-    println!("");
-
-    let video = info.best_av_format().unwrap();
-    println!("{}", video);
-
-    let audio = info.best_audio_format().unwrap();
-    println!("{}", audio);
-
-    println!("abr {}", FFMpeg::round_mp3_bitrate(129.492));
-
-    FFMpeg::convert_to_mp3("audio.m4a", "audio.mp3", 160)
-        .await
-        .expect("convert");
-
+    bot_main().await?;
     Ok(())
-    //bot_main().await
 }
