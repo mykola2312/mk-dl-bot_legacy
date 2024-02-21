a3217274e3a6d20d66ce19a23a81745372be91c5
mykola2312 <nikolay_tihonov@i.ua>
2024-02-21 18:48:03 +0200

    begin implementing generic download function

--- src/dl.rs
+++ src/dl.rs
@@ -1,3 +1,27 @@
+use self::spawn::SpawnError;
+use self::yt_dlp::YtDlpError;
+
 pub mod ffmpeg;
 mod spawn;
 pub mod yt_dlp;
+
+pub enum DownloadError {
+    Message(String)
+}
+
+impl From<SpawnError> for DownloadError {
+    fn from(value: SpawnError) -> Self {
+        Self::Message(value.to_string())
+    }
+}
+
+impl From<YtDlpError> for DownloadError {
+    fn from(value: YtDlpError) -> Self {
+        Self::Message(value.to_string())
+    }
+}
+
+pub async fn download(url: &str) -> Result<String, DownloadError> {
+
+    todo!()
+}
