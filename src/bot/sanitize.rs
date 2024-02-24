9420ad681b0d1379bbfd7fa5cc16ecbbcc1af291
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-02-24 15:20:38 +0200

    write function to parse url via url crate

--- src/bot/sanitize.rs
+++ src/bot/sanitize.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use url::Url;
 
 // https://stackoverflow.com/questions/6038061/regular-expression-to-find-urls-within-a-string
 const RE_URL: &str =
@@ -12,16 +13,27 @@ pub fn extract_url(text: &str) -> Option<&str> {
     }
 }
 
+pub fn parse_url(url: &str) -> Option<Url> {
+    Url::parse(url).ok()
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::bot::sanitize::extract_url;
+    use crate::bot::sanitize::{extract_url, parse_url};
 
     #[test]
     fn test_extract_url() {
-        // https://www.youtube.com/watch?v=00000000000
-
         assert_eq!(extract_url("test http://www.test.com/id/1"), Some("http://www.test.com/id/1"));
         assert_eq!(extract_url("https://www.test.com 3"), Some("https://www.test.com"));
         assert_eq!(extract_url("there is no any url"), None);
+    }
+
+    #[test]
+    fn test_parse_url() {
+        let url = parse_url("https://www.youtube.com/watch?v=00000000000").unwrap();
+        assert_eq!(url.host_str().unwrap(), "www.youtube.com");
+
+        let url = parse_url("https://youtu.be/00000000000").unwrap();
+        assert_eq!(url.host_str().unwrap(), "youtu.be");
     }
 }
