d68cb90b3beb099e045e5a0ae9505d8a28df3f9a
mykola2312 <nikolay_tihonov@i.ua>
2024-03-01 01:17:48 +0200

    make first migration to implement database

---
+++ migrations/1_init.sql
@@ -0,0 +1,44 @@
+CREATE TABLE "user"
+(
+    id                  INTEGER PRIMARY KEY AUTOINCREMENT,
+    tg_id               INTEGER NOT NULL UNIQUE,
+    can_download        INTEGER NOT NULL,
+    is_admin            INTEGER NOT NULL
+);
+
+CREATE TABLE "chat"
+(
+    tg_id               INTEGER PRIMARY KEY,
+    can_download        INTEGER NOT NULL
+);
+
+CREATE TABLE "link"
+(
+    id                  INTEGER PRIMARY KEY AUTOINCREMENT,
+    domain              TEXT    NOT NULL UNIQUE,
+    path                TEXT,
+    download_allowed    INTEGER NOT NULL,
+    auto_download       INTEGER NOT NULL
+);
+
+CREATE INDEX idx_link_domain
+    ON "link"(domain);
+
+CREATE TABLE "request"
+(
+    id                  INTEGER PRIMARY KEY AUTOINCREMENT,
+    requested_by        INTEGER NOT NULL UNIQUE,
+    approved_by         INTEGER UNIQUE,
+    message             TEXT,
+    is_approved         INTEGER NOT NULL,
+
+    FOREIGN KEY(requested_by)   REFERENCES "user"(id),
+    FOREIGN KEY(approved_by)    REFERENCES "user"(id)
+);
+
+CREATE TRIGGER "approve"
+AFTER UPDATE OF is_approved ON "request"
+WHEN new.is_approved = 1
+BEGIN
+    UPDATE user SET can_download = 1 WHERE user.id = new.requested_by;
+END;
