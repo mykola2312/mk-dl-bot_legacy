e692ebceb8fc37101930cec1968b4ceb7d92046f
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-03-03 18:54:26 +0200

    introduct macro to automatically reply with i18n message and return from command handler

--- src/bot.rs
+++ src/bot.rs
@@ -2,5 +2,14 @@ pub mod bot;
 pub mod dl;
 pub mod op;
 pub mod sanitize;
+pub mod request;
 pub mod start;
 pub mod types;
+
+#[macro_export]
+macro_rules! reply_i18n_and_return {
+    ($bot:expr, $chat_id:expr, $line:expr) => {
+        $bot.send_message($chat_id, t!($line)).await?;
+        return Ok(())
+    }
+}
