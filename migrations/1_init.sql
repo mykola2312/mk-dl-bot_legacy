bc045e9b72fcbd86b52125edb7f46b5b140fc18d
mykola2312 <nikolay_tihonov@i.ua>
2024-03-03 18:05:16 +0200

    add request_chat table for requesting access to download for chats

--- migrations/1_init.sql
+++ migrations/1_init.sql
@@ -49,3 +49,24 @@ WHEN new.is_approved = 1
 BEGIN
     UPDATE user SET can_download = 1 WHERE user.id = new.requested_by;
 END;
+
+CREATE TABLE "request_chat"
+(
+    id                  INTEGER PRIMARY KEY AUTOINCREMENT,
+    requested_by        INTEGER NOT NULL UNIQUE,
+    requested_for       INTEGER NOT NULL UNIQUE,
+    approved_by         INTEGER UNIQUE,
+    message             TEXT,
+    is_approved         INTEGER NOT NULL,
+
+    FOREIGN KEY(requested_by)   REFERENCES "user"(id),
+    FOREIGN KEY(requested_for)  REFERENCES "chat"(id),
+    FOREIGN KEY(approved_by)    REFERENCES "user"(id)
+);
+
+CREATE TRIGGER "approve_chat"
+AFTER UPDATE OF is_approved ON "request_chat"
+WHEN new.is_approved = 1
+BEGIN
+    UPDATE chat SET can_download = 1 WHERE chat.id = new.requested_for;
+END;
