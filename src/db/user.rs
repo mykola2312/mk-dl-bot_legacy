38696fa2b4578886a99d4d22266dbbf2f960e6e7
mykola2312 <nikolay_tihonov@i.ua>
2024-03-08 08:20:50 +0200

    fix sql errors

--- src/db/user.rs
+++ src/db/user.rs
@@ -3,12 +3,7 @@ use teloxide::types;
 use super::{DbPool, User};
 use crate::unwrap_or_create;
 
-pub async fn create_user(
-    db: &DbPool,
-    user: &types::User,
-    can_download: bool,
-    is_admin: bool,
-) -> Result<User, sqlx::Error> {
+pub async fn create_user(db: &DbPool, user: &types::User) -> Result<User, sqlx::Error> {
     sqlx::query(
         r#"INSERT OR IGNORE INTO "user"
         (tg_id, username, first_name, last_name, can_download, is_admin, has_private_chat)
@@ -18,8 +13,8 @@ pub async fn create_user(
     .bind(&user.username)
     .bind(&user.first_name)
     .bind(&user.last_name)
-    .bind(can_download as i64)
-    .bind(is_admin as i64)
+    .bind(false)
+    .bind(false)
     .bind(0)
     .execute(db)
     .await?;
@@ -38,5 +33,5 @@ pub async fn find_or_create_user(db: &DbPool, user: &types::User) -> Result<User
             .fetch_one(db)
             .await;
 
-    unwrap_or_create!(db, user, res, create_user, false, false)
+    unwrap_or_create!(db, user, res, create_user)
 }
